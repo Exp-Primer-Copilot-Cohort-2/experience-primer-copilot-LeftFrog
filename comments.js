497cad04b423a23addc4eea1f7f0ab63a8ff4cca
LeftFrog <125793934+LeftFrog@users.noreply.github.com>
2024-07-11 17:58:23 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,27 @@
+//Create web server
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let comments = [];
+
+app.get('/comments', (req, res) => {
+    res.json(comments);
+});
+
+app.post('/comments', (req, res) => {
+    const comment = req.body;
+    comments.push(comment);
+    res.json(comment);
+});
+
+app.listen(3000, () => {
+    console.log('Server started');
+});
+
+//Create a file to store the comments
+fs.writeFileSync('comments.json', JSON.stringify(comments), 'utf8');
