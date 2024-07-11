7ce48d944a8720214c3f2e953ea26974ee73a421
LeftFrog <125793934+LeftFrog@users.noreply.github.com>
2024-07-11 17:53:43 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillMember() {
+    var member = document.getElementById("member");
+    member.style.display = "block";
+    var skill = document.getElementById("skill");
+    skill.style.display = "none";
+    var project = document.getElementById("project");
+    project.style.display = "none";
+    var about = document.getElementById("about");
+    about.style.display = "none";
+}
